e847bb9a50265333c6a629b180b335c7affd7787
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-06-23 23:44:42 +0530

    complete Day 1

---
+++ day-1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to brand name generator!!")
+city = input("Which city you grew up in? ")
+pet = input("What is your pet's name? ")
+brand_name = city + " " + pet
+print(f'Your brand name should be "{brand_name}"!!')
